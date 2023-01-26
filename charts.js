b5f8f3cc0665be6c72ac607719894f2d1090628c
Destin Kronenwetter <destinkronen@gmail.com>
2023-01-25 21:26:32 -0700

    submission 2 wo website customization

--- charts.js
+++ charts.js
@@ -80,7 +80,7 @@ function buildCharts(sample) {
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = otuIds.slice(0,10).map(otuID => 'OTU ${otuID}').reverse();
+    let yticks = otuIds.slice(0,10).map(otuID => otuID.reverse());
     console.log(yticks)
     // Deliverable 1: 8. Create the trace for the bar chart. 
     var barData = [{
